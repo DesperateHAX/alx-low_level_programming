2eea91a1d1f724cdc3022f51da69cd7a966f4042
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-12 16:42:24 -0700

    Task 9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+
 /**
- * main -- Entry point
+ * main - Entry point
  *
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
- * Return: 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int n = '0';
