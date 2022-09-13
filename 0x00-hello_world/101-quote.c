05d814e2a5e3779349a5edf9f778109819562831
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-12 17:46:43 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,20 +4,18 @@
 
 /**
  *
- *  * main - Prints output
+ * main - Prints output
  *
- *   *
  *
- *    * Return: Always 0
+ * Return: Always 0
  *
- *     */
+ */
 
 int main(void)
 
 {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-			return (1);
+	return (1);
 
 }
