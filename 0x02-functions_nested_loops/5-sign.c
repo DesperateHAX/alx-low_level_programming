3aa1af3ff65dff908a55edd61bbf2b43eb5ddf47
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-13 20:24:15 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,15 @@
 int print_sign(int n)
 {
 	if (n > 0)
-
-		_putchar(43 + 0);
+	{
+		_putchar('+');
 		return (1);
-	
-	else if (n == 0)
-
-		_putchar(0 + 48);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
 		return (0);
-	else
-
-		_putchar(45 + 0);
-		return (-1);
-
+	}
+	_putchar('-');
+	return (-1);
 }
-	
