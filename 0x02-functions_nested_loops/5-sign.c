b9a6776cfb8aaf75d5d6d4b9126ed8c24cfb48a1
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-13 19:58:49 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,35 +1,35 @@
-#include "main.h"                                                                                                                       
+#include "main.h"
 
-/**                                                                                                                                     
+/**
  *
- * print_sign - main function                                                                                                           
+ * print_sign - main function
  *
- * @n: character to compare                                                                                                             
+ * @n: character to compare
  *
- * Return: 1 if positive, 0 if zero, -1 if negative                                                                                     
+ * Return: 1 if positive, 0 if zero, -1 if negative
  *
- */                                                                                                                                     
+ */
 
-int print_sign(int n)                                                                                                                   
+int print_sign(int n)
 
-{                                                                                                                                       
+{
 
-	        if (n > 0)                                                                                                                      
-      		{                                                                                                                   
-              		_putchar('+');                                                                                                          
-                                                                                                                                      
-	                return (1);                                                                                                             
-	        }                                                                                                                               
+	        if (n > 0)
+      		{
+              		_putchar('+');
 
-	        if (n == 0)                                                                                                                  
-	        {                                                                                                                               
-	                _putchar('0');                                                                                                          
+	                return (1);
+	        }
 
-	                return (0);                                                                                                             
+	        if (n == 0)
+	        {
+	                _putchar('0');
+
+	                return (0);
 		}
-		                                                                                                                              
 
-	        _putchar('-');                                                                                                                  
-	        return (-1);                                                                                                                    
+
+	        _putchar('-');
+	        return (-1);
 }
-					
+
