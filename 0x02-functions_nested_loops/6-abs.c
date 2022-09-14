1bab13d4813cc0118735c612db97711be5922372
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-13 20:41:20 -0700

    Task  6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ * _abs - function that makes numbers absolute 
+ * @n: takes in integer type input for function
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
