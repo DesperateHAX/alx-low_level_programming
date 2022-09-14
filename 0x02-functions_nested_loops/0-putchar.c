ae71af728091a9995f505e8f36b8168ab6432f34
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-13 17:50:43 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+/**
+ * main - Entry poin
+ * Description: We are trying to print putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+
+		char ouput[] = "_putchar";
+
+		int i = 0;
+
+
+		for (i < 8; ++i)
+
+			_putchar(str[i]);
+
+		_putchar('\n');
+		return (0);
+
+}
