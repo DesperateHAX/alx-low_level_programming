8165e449ceb87e5762937d9242a0f5828f9e615d
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-16 10:46:13 -0700

    Task 5

---
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+ /**
+ * main - check the code
+ *
+ * 4-main.c -  a function that prints the numbers, from 0 to 9,
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
