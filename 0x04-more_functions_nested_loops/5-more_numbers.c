f9d4f9e18c099cb975ff027ed7447316b3fe5baa
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-16 11:53:05 -0700

    Task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *5-more_numbers - print 0 - 14 ten times and you
+ * 5-more_numbers - print 0 - 14 ten times and you
  *	can only use _putchar three times
  *
  * Return: Always 0
