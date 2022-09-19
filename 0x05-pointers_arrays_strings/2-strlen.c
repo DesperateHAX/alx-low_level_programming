5ad816b4635ed87f6b6a0a987fe0df416d438a55
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-19 16:20:35 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 2-strlen - Calculating the lenghth of a string
+ * @s: A pointer that evaluates a string
+ * Return: Always 0
+ */
+int _strlen(char *s);
+{
+	int i = 0;
+
+	while (*s[i] != "\0")
+	{
+		i++;
+	}
+
+	return (i);
+}
