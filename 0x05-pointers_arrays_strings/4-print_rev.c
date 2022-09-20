2ef1d6840d344b31ebe4f07a61c8222ded3e1532
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-19 18:54:03 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - reverse a string
+ *
+ *
+ *
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlens(s) - 1;
+
+	for (i >= 0; i--)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+
+}
