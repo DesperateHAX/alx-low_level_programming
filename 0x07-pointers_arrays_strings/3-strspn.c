ceaad5641eed0f2a44b93c781770e0ab04ba105f
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-26 17:31:40 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,35 +14,25 @@ unsigned int _strspn(char *s, char *accept)
 
 	int i, j, f;
 
-
-
 	i = 0;
 
 	while (s[i] != '\0')
-
 	{
 		f = 1;
 		j = 0;
 
 		while (accept[j] != '\0')
-
 		{
-
 			if (s[i] == accept[j])
-
 			{
 
 				f = 0; /*success*/
-
 				break;
-
 			}
 
 			j++;
-
 		}
+	}
 
-		if (f == 1)
-
-			break;
+	return (i);
 }
