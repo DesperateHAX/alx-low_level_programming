6c62354eb4e11d0ae033b866134b81b25a1a9180
Okuhle Pepu <okuhlepepu@gmail.com>
2022-09-26 20:22:08 -0700

    Tasl 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,38 +10,26 @@
  *         NULL if substring is not found
  */
 
-char *_strstr(char *haystack, char *needle)
+har *_strstr(char *haystack, char *needle)
 
 {
-	char *h, *n;
+	int i;
+	int j;
 
-	while (*haystack != '\0')
-
+	if (needle[0] == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-
-		h = haystack;
-
-		n = needle;
-
-		while (*n != '\0' && *haystack == *n)
-
+		if (haystack[i] == needle[0])
 		{
-
-			haystack++;
-
-			n++;
-
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+			}
+			if (needle[j] == '\0')
+				return (haystack + i);
 		}
-
-		if (!*n)
-
-			return (h);
-
-		haystack++;
-
 	}
-
 	return ('\0');
-}
-
 }
